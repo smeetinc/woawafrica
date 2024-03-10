cbef91b72e2eef2bc51dcedb6ab1a8f9c55c06e9
Oluwaseyi Smith Bayode <bayodeoluseyi@gmail.com>
2024-03-10 18:40:01 +0100

    testing destination page

---
+++ src/pages/Destinations.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Destinations() {
+  return (
+    <div className="py-10">
+      <div className="mt-40 text-black font-bold z-20">Destinations</div>
+    </div>
+  );
+}
+
+export default Destinations;
